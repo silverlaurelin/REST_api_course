25e5bd0d16e75d49fb6223f1382ee539fe2341b4
tykhonova <liubov.tykhonova@globallogic.com>
2017-01-06 11:17:50 +0200

    added db, store api

--- app.py
+++ app.py
@@ -1,20 +1,31 @@
-from flask import Flask, request
+from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
 
 from security import authenticate, identity
-from user import UserRegister
-from item import Item, ItemList
+from resources.user import UserRegister
+from resources.item import Item, ItemList
+from resources.store import Store, StoreList
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'jose'
 api = Api(app)
 
-jwt = JWT(app, authenticate, identity)
+@app.before_first_request
+def create_tables():
+    db.create_all()
 
+jwt = JWT(app, authenticate, identity)  # /auth
+
+api.add_resource(Store, '/store/<string:name>')
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
+api.add_resource(StoreList, '/stores')
 api.add_resource(UserRegister, '/register')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    from db import db
+    db.init_app(app)
+    app.run(port=5000, debug=True)
